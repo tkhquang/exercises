2fdf5099e218b0ed114272a13aa52bcdfaa8567a
Quang Trinh <khacquang.trinh@gmail.com>
2020-07-05 23:08:49 +0700

    Create lib.rs

---
+++ rust/1 - Armstrong Numbers/lib.rs
@@ -0,0 +1,14 @@
+pub fn is_armstrong_number(num: u32) -> bool {
+    let num_str = num.to_string();
+
+    // num_str
+    //     .chars()
+    //     .map(|c| c.to_digit(10).unwrap().pow(num_str.len() as u32))
+    //     .collect::<Vec<_>>()
+    //     .iter()
+    //     .sum::<u32>() == num
+
+    num_str
+        .chars()
+        .fold(0, |sum, c| sum + c.to_digit(10).unwrap().pow(num_str.len() as u32)) == num
+}
