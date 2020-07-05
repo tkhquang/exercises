05b669d1e2e142949492a8f328dbe4174083b86b
Quang Trinh <khacquang.trinh@gmail.com>
2020-07-06 02:58:40 +0700

    Update lib.rs

--- rust/0 - Raindrops/lib.rs
+++ rust/0 - Raindrops/lib.rs
@@ -1,21 +1,21 @@
 pub fn raindrops(n: u32) -> String {
-    let mut result: String = "".to_owned();
+    let mut result: String = String::new();
 
     if n % 3 == 0 {
-      result.push_str("Pling");
+        result.push_str("Pling");
     }
 
     if n % 5 == 0 {
-      result.push_str("Plang");
+        result.push_str("Plang");
     }
 
     if n % 7 == 0 {
-      result.push_str("Plong");
+        result.push_str("Plong");
     }
 
-    if result == "" {
-      result.push_str(&n.to_string());
+    if result.is_empty() {
+        result.push_str(&n.to_string());
     }
     
-    return result;
+    result
 }
