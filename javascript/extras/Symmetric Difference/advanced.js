0beb761c7831bf85645dcc8226c9892b1f180cc3
Quang Trinh <khacquang.trinh@gmail.com>
2020-07-14 00:03:34 +0700

    Create advanced.js

---
+++ javascript/extras/Symmetric Difference/advanced.js
@@ -0,0 +1,20 @@
+function sym (...args) {
+  return [...args.reduce(reducer, new Set())];
+}
+
+function reducer (result, arr) {
+  const compare = new Set(arr);
+  
+  for (let val of compare) {
+    if(result.has(val)) {
+      result.delete(val);
+    } else {
+      result.add(val);
+    }
+  }
+
+  return result;
+}
+
+// Tests
+sym([1, 2, 3], [5, 2, 1, 4]);
